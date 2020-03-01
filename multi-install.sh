9497597e407b6cf1ea75ba5ebdf8b9487bbea85c
Mobulos <Fabian.schmeltzer77@outlook.de>
2020-03-01 18:39:48 +0100

    Update multi-install.sh

--- multi-install.sh
+++ multi-install.sh
@@ -1,25 +1,93 @@
 #!/bin/bash
 # Init
 
-# LADE "jumpto"
+function menue() {
+	set -u
+	clear
+	echo "$yellow########################################"
+	sleep .1
+	echo "###  Multi-install Script by Mobulos ###"
+	sleep .1
+	echo "########################################"
+	sleep .1
+	echo
+	echo "Version 0.0.2"
+	echo "Update 01.03.2020" #TODO Version und Datum ändern
+	echo "$reset"
+	echo
+	log_warning "Dies ist die PRE-RELEASE Version, das Script verfügt noch nicht über alle Funktionen!"
+	echo
+	echo
+	sleep .5
+	echo "Auswahlmöglichkeiten:"
+	sleep .1
+	tmp=($(tput setaf 3))
+	echo -n "$tmp"
+	echo "[1] soon"
+	tmp=($(tput setaf 4))
+	echo -n "$tmp"
+	sleep .1
+	echo "[2] Update"
+	tmp=($(tput setaf 5))
+	echo -n "$tmp"
+	sleep .1
+	echo "[3] Exit"
+	read -n1 -p "Was willst du tun?: " befehl
+	clear
+	echo -n "$reset"
+	case $befehl in
+	1)
+		soon
+		;;
+	2)
+		rm 20*
+		update
+		;;
+	3)
+		exit
+		;;
+	*)
+		clear
+		log_error "Du musst dich vertippt haben..."
+		menue
+		;;
+	esac
 
-function jumpto() {
-	label=$1
-	cmd=$(sed -n "/$label:/{:a;n;p;ba};" $0 | grep -v ':$')
-	eval "$cmd"
-	exit
 }
 
-# LEGE JUMPTOs AN
-menue=${1:-"menue"}
-update=${2:-"update"}
+function update() {
+	# CHECK, OB DAS SCRIPT HEUTE UPGEDATED WURDE
+	if [ -f $(date +%Y-%m-%d) ]; then
+		# WENN HEUTE BEREITS UPGEDATED GEHE ZUM MENÜ
+		menue
+	elif [[ * ]]; then
+		# WENN HEUTE NICHT UPGEDATED GEHE WEITER
+		# LÖSCHE "ZULETZT UPGEDATED" DATEI
+		touch "$(date +%Y-%m-%d)"
+		rm 20* || :
+		clear
+		echo "$red Die neuste Version wird heruntergeladen"
+		curl --progress-bar https://raw.githubusercontent.com/Mobulos/multi-install/master/multi-install.sh -o multi-install.sh
+		sleep 2
+		# wget https://raw.githubusercontent.com/Mobulos/multi-install/master/multi-install.sh
+		echo "$reset"
+		sleep .5
+		clear
+		log_success "Das Update wurde Erfolgreich heruntergeladen!"
+		sleep 1
+		chmod +x multi-install.sh
+	fi
+	touch "$(date +%Y-%m-%d)"
+	./multi-install.sh
+
+}
 
 function soon() {
 	clear
 	log_warning "Diese Funktion wird später hinzugefügt!"
 	read -n1
 	clear
-	jumpto $menue
+	menue
 }
 
 particular_script() {
@@ -64,86 +132,3 @@ elif [[ * ]]; then
 		clear
 	done
 fi
-
-# DAS MENÜ
-
-menue:
-set -u
-clear
-echo "$yellow########################################"
-sleep .1
-echo "###  Multi-install Script by Mobulos ###"
-sleep .1
-echo "########################################"
-sleep .1
-echo
-echo "Version 0.0.1"
-echo "Update 01.03.2020" #TODO Version und Datum ändern
-echo "$reset"
-echo
-log_warning "Dies ist die PRE-RELEASE Version, das Script verfügt noch nicht über alle Funktionen!"
-echo
-echo
-sleep .5
-echo "Auswahlmöglichkeiten:"
-sleep .1
-tmp=($(tput setaf 3))
-echo -n "$tmp"
-echo "[1] soon"
-tmp=($(tput setaf 4))
-echo -n "$tmp"
-sleep .1
-echo "[2] Update"
-tmp=($(tput setaf 5))
-echo -n "$tmp"
-sleep .1
-echo "[3] Exit"
-read -n1 -p "Was willst du tun?: " befehl
-clear
-echo -n "$reset"
-case $befehl in
-1)
-	soon
-	;;
-2)
-	clear
-	jumpto update
-	;;
-3)
-	exit
-	;;
-*)
-	clear
-	log_error "Du musst dich vertippt haben..."
-	jumpto $menue
-	;;
-esac
-
-# log_success "test"
-exit
-
-# UPDATE DAS SCRIPT
-update:
-# CHECK, OB DAS SCRIPT HEUTE UPGEDATED WURDE
-if [ -f $(date +%Y-%m-%d) ]; then
-	# WENN HEUTE BEREITS UPGEDATED GEHE ZUM MENÜ
-	jumpto $menue
-elif [[ * ]]; then
-	# WENN HEUTE NICHT UPGEDATED GEHE WEITER
-	# LÖSCHE "ZULETZT UPGEDATED" DATEI
-	touch "$(date +%Y-%m-%d)"
-	rm 20* || :
-	clear
-	echo "$red Die neuste Version wird heruntergeladen"
-	curl --progress-bar https://raw.githubusercontent.com/Mobulos/multi-install/master/multi-install.sh -o multi-install.sh
-	sleep 2
-	# wget https://raw.githubusercontent.com/Mobulos/multi-install/master/multi-install.sh
-	echo "$reset"
-	sleep .5
-	clear
-	log_success "Das Update wurde Erfolgreich heruntergeladen!"
-	sleep 1
-	chmod +x multi-install.sh
-fi
-touch "$(date +%Y-%m-%d)"
-./multi-install.sh
