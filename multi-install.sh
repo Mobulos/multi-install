94287008edf8b9437346356ca683e2041e553037
Mobulos <Fabian.schmeltzer77@outlook.de>
2020-03-01 18:41:59 +0100

    Update multi-install.sh

--- multi-install.sh
+++ multi-install.sh
@@ -11,7 +11,7 @@ function menue() {
 	echo "########################################"
 	sleep .1
 	echo
-	echo "Version 0.0.2"
+	echo "Version 0.0.3"
 	echo "Update 01.03.2020" #TODO Version und Datum ändern
 	echo "$reset"
 	echo
