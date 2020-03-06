add7e04c11174bdfbd587f7511004403eaf2d540
Mobulos <Fabian.schmeltzer77@outlook.de>
2020-03-06 16:32:31 +0100

    Revert "Test usage"
    This reverts commit 19d0e9de078e8fb975107112bfeafe351f94fd2c.

--- multi-install.sh
+++ multi-install.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 # Init
 
-# Print usage
-usage() {
-  echo -n "${scriptName} [OPTION]... [FILE]...
-This is a script template.  Edit this description to print help to users.
- ${bold}Options:${reset}
-  -u, --username    Username for script
-  -p, --password    User password
-  --force           Skip all user interaction.  Implied 'Yes' to all actions.
-  -q, --quiet       Quiet (no output)
-  -l, --log         Print log to file
-  -s, --strict      Exit script with null variables.  i.e 'set -o nounset'
-  -v, --verbose     Output more information. (Items echoed to 'verbose')
-  -d, --debug       Runs script in BASH debug mode (set -x)
-  -h, --help        Display this help and exit
-      --version     Output version information and exit
-"
-}
 
 # LADE DAS LOG FEATURE
 rm .log4bash.sh
