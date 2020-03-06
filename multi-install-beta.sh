c64ee50bbc800017967674387c60a44fdc0b54fd
Mobulos <Fabian.schmeltzer77@outlook.de>
2020-03-06 23:59:35 +0100

    Update Fix | Code Improvement
    Das Updaten wurde behoben
    Abschnitte zum besseren Verständnis hinzugefügt
    Farben im Menü wurden Verbessert
    
    Behobene Fehler:
    - Updaten ist nun Funktionsfähig
    
    Verbesserungen:
    - Farben im Menü wurden Optimiert
    - Kommentare zur Besseren Navigation im Menü wurden hinzugefügt

--- multi-install-beta.sh
+++ multi-install-beta.sh
@@ -22,7 +22,7 @@ This is a script template.  Edit this description to print help to users.
 
 ############################################
 ################# CHANGE ###################
-ver=0.1.3
+ver=0.1.4
 dat=06.03.2020
 ############################################
 ############################################
@@ -58,8 +58,8 @@ function pre () {
 	echo "##########################################"
 	sleep .1
 	echo
-	echo "Version $ver"
-	echo "Update $dat" #TODO Version und Datum ändern
+	echo "$red DEVELOPER'$reset' Version $ver"
+	echo "Update $dat"
 	echo "$reset"
 	echo
 	log_warning "Dies ist die PRE-RELEASE Version, das Script verfügt noch nicht über alle Funktionen!"
@@ -68,7 +68,14 @@ function pre () {
 
 }
 
+#   __  __   _____   _   _   _   _   _____ 
+#  |  \/  | | ____| | \ | | | | | | | ____|
+#  | |\/| | |  _|   |  \| | | | | | |  _|  
+#  | |  | | | |___  | |\  | | |_| | | |___ 
+#  |_|  |_| |_____| |_| \_|  \___/  |_____|
 
+
+                            
 function menue () {
 	set -u
 	clear
@@ -76,23 +83,24 @@ function menue () {
 	sleep .5
 	echo "Auswahlmöglichkeiten:"
 	sleep .1
-	tmp=($(tput setaf 3))
-	echo -n "$tmp"
-	echo "[1] Einstellungen"
 	tmp=($(tput setaf 4))
 	echo -n "$tmp"
-	sleep .1
-	echo "[2] Update"
+	echo "[1] Einstellungen"
 	tmp=($(tput setaf 5))
 	echo -n "$tmp"
 	sleep .1
+	echo "[2] Update"
+	tmp=($(tput setaf 6))
+	echo -n "$tmp"
+	sleep .1
 	echo "[3] Exit"
+	tmp=($(tput setaf 3))
+	echo -n "$tmp"
 	read -n1 -p "Was willst du tun?: " befehl
 	clear
 	echo -n "$reset"
 	case $befehl in
 	1)
-		clear
 		settings
 		;;
 	2)
@@ -108,6 +116,7 @@ function menue () {
 	*)
 		clear
 		log_error "Du musst dich vertippt haben..."
+		read -t2 -n1
 		menue
 		;;
 	esac
@@ -117,8 +126,29 @@ function menue () {
 function settings () {
 	pre
 	sleep .5
-	echo "Auswahlmöglichkeiten:"	
+	 echo "  _____   ___   _   _   ____    _____   _____   _       _       _   _   _   _    ____   _____   _   _ 
+			| ____| |_ _| | \ | | / ___|  |_   _| | ____| | |     | |     | | | | | \ | |  / ___| | ____| | \ | |
+			|  _|    | |  |  \| | \___ \    | |   |  _|   | |     | |     | | | | |  \| | | |  _  |  _|   |  \| |
+			| |___   | |  | |\  |  ___) |   | |   | |___  | |___  | |___  | |_| | | |\  | | |_| | | |___  | |\  |
+			|_____| |___| |_| \_| |____/    |_|   |_____| |_____| |_____|  \___/  |_| \_|  \____| |_____| |_| \_|
+                                                                                                      
+"
+	echo
+	echo
+	echo
+		
+	read -n1
 }
+
+
+
+#   _   _   ____    ____       _      _____   _____ 
+#  | | | | |  _ \  |  _ \     / \    |_   _| | ____|
+#  | | | | | |_) | | | | |   / _ \     | |   |  _|  
+#  | |_| | |  __/  | |_| |  / ___ \    | |   | |___ 
+#   \___/  |_|     |____/  /_/   \_\   |_|   |_____|
+                                                  
+
 
 function update () {
 	# CHECK, OB DAS SCRIPT HEUTE UPGEDATED WURDE
@@ -137,7 +167,7 @@ function update () {
 		wget https://raw.githubusercontent.com/Mobulos/multi-install/develop/multi-install-beta.sh
 		sleep 2
 		echo "$reset"
-		sleep .5
+		mv multi-install-beta.sh.1 multi-install-beta.sh
 		clear
 		log_success "Das Update wurde Erfolgreich heruntergeladen!"
 		sleep 1
@@ -147,6 +177,14 @@ function update () {
 	./multi-install-beta.sh
 
 }
+
+#   ____     ___     ___    _   _ 
+#  / ___|   / _ \   / _ \  | \ | |
+#  \___ \  | | | | | | | | |  \| |
+#   ___) | | |_| | | |_| | | |\  |
+#  |____/   \___/   \___/  |_| \_|
+                                
+
 
 function soon () {
 	clear
@@ -169,7 +207,8 @@ elif [[ * ]]; then
 	apt-get upgrade -y
 
 	# INSTALLATION BENÖTIGTER PAKETE
-	for i in curl wget sudo screen; do
+	for i in curl wget sudo screen
+	do
 		apt-get install $i -y
 		clear
 	done
